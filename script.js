e1191ee05f7696faeffc20ab0556922076642ea4
Johanan Samuel <johanansamuel76@gmail.com>
2023-11-26 20:24:48 +0800

    finish project

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const imageContainerEl = document.querySelector('.image-container');
+const prevEl = document.getElementById('prev');
+const nextEl = document.getElementById('next');
+let x = 0;
+let timer;
+
+prevEl.addEventListener('click', ()=> {
+    x += 45;
+    clearTimeout(timer)
+    updateGallery();
+})
+
+nextEl.addEventListener('click', ()=> {
+    x -= 45;
+    clearTimeout(timer)
+    updateGallery();
+})
+
+function updateGallery() {
+    imageContainerEl.style.transform = `perspective(1000px) rotateY(${x}deg)`;
+    timer = setTimeout(()=> {
+        x -= 45
+        updateGallery()
+    }, 3000)
+}
+
+updateGallery();
